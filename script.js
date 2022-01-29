cfad8e1dc131569ff00968444e1afba8b2e093e3
Valentin <93974733+valentindush@users.noreply.github.com>
2022-01-29 13:20:30 +0200

    giant brain

---
+++ script.js
@@ -0,0 +1,15 @@
+
+let displayArea = document.querySelector('.display-area')
+
+function display(number){
+    displayArea.innerHTML += number
+
+    return
+}
+
+
+
+const add = document.querySelector('#add')
+const sub = document.querySelector('#sub')
+const multiply = document.querySelector('#times')
+const divide = document.querySelector('#divide')
